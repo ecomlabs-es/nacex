4e7257ed3a0a3543297e2ec7d56f5e0003c00ed3
Josean <info@ecomlabs.es>
2026-04-09 00:29:18 +0200

    Fix COnxShop.php: validate postcode with Validate::isPostCode, use Tools::getValue
    - Replace $_GET['cp'] with Tools::getValue() + Validate::isPostCode()
    - Fix charset from iso-8859-1 to utf-8
    - Fix broken $this->l() call (no context in standalone script)
    - Remove empty script block and extra closing tag

--- COnxShop.php
+++ COnxShop.php
@@ -1,12 +1,17 @@
 <?php
 include(dirname(__FILE__) . '/../../config/config.inc.php');
 include(dirname(__FILE__) . '/../../init.php');
+
+$cp = Tools::getValue('cp', '');
+if (!Validate::isPostCode($cp) || empty($cp)) {
+    die('Invalid postcode');
+}
 ?>
     <html>
     <head>
-        <meta http-equiv='content-type' content='text/html;charset=iso-8859-1'>
+        <meta http-equiv='content-type' content='text/html;charset=utf-8'>
     </head>
-    <h1 id="cabecerashop" style="color:orange;"><?php echo $this->l('Nacexshop delivery points') ?></h1>
+    <h1 id="cabecerashop" style="color:orange;">Nacexshop</h1>
     </body>
     <link type='text/css' rel='stylesheet'
           href='<?php echo _PS_BASE_URL_ . __PS_BASE_URI__ ?>modules/nacex/css/nacex.css'/>
@@ -18,7 +23,7 @@ require_once(dirname(__FILE__) . '/nacexWS.php');
 $_nacex_shop = new nacexshop();
 $_ws = new nacexWS();
 //GET AGENCIA COORDENADAS
-$_coordenadas = $_ws->get_Agencia3($_GET['cp']);
+$_coordenadas = $_ws->get_Agencia3($cp);
 $_coordenadas = $_ws->treatmentXML($_coordenadas, 'getAgencia3');
 
 /** Añadimos funcionalidad WS sin conexión **/
@@ -34,7 +39,7 @@ if ($_coordenadas[0] != '500ERROR') {
     $_nacex_shop->add_nacex_shop_name($_agencias);
 } else { // Si no hay conexión WS
     $conn = false;
-    $return = [$_GET['cp']];
+    $return = [$cp];
     $_agencias = $_nacex_shop->getAgenciasTratadas($return);
 }
 
@@ -53,20 +58,9 @@ if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
 
 //MAP OR LIST
 if (Configuration::get('NACEX_GOOGLE_API') != '' && is_null($bname)) {
-    echo $_nacex_shop->nacex_shop_map($_agencias, $_GET['cp'], $conn);
+    echo $_nacex_shop->nacex_shop_map($_agencias, $cp, $conn);
 } else {
-    echo $_nacex_shop->nacex_shop_list($_agencias, $_GET['cp'], $conn);
+    echo $_nacex_shop->nacex_shop_list($_agencias, $cp, $conn);
 }
-/*if (Configuration::get('NACEX_GOOGLE_API') != "") {
-    echo $_nacex_shop->nacex_shop_map($_lat, $_long, $_agencias, $_GET['cp']);
-} else {
-    echo $_nacex_shop->nacex_shop_list($_agencias, $_GET['cp']);
-}*/
 ?>
-    <script type='text/javascript'>
-
-    </script>
     </html>
-<?php
-
-?>
