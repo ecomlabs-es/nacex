84a1076b2f5f85f20655576d57add6d433ef5c90
Josean <info@ecomlabs.es>
2026-04-09 14:38:04 +0200

    Fix hash: reuse existing hash instead of regenerating on each request
    PS8 triggers the hook in two separate requests per page load.
    The second request was overwriting the hash from the first, causing
    validation to always fail. Now we reuse the stored hash if one exists.

--- hash.php
+++ hash.php
@@ -2,8 +2,6 @@
 
 class hash
 {
-    private static $requestCache = [];
-
     private static function getStoredHashes()
     {
         if (session_status() === PHP_SESSION_NONE) {
@@ -24,35 +22,24 @@ class hash
 
     public static function hash_form($order_id)
     {
-        if (isset(self::$requestCache[$order_id])) {
-            return self::$requestCache[$order_id];
-        }
-
-        $rand = random_int(100000, PHP_INT_MAX);
         $hashes = self::getStoredHashes();
 
-        $found = false;
-        foreach ($hashes as $key => $entry) {
+        // Si ya existe un hash para este pedido, reutilizarlo
+        foreach ($hashes as $entry) {
             if ($entry['ORDER_ID'] == $order_id) {
-                $hashes[$key]['HASH'] = $rand;
-                $found = true;
-                break;
+                return $entry['HASH'];
             }
         }
 
-        if (!$found) {
-            $hashes[] = ['HASH' => $rand, 'ORDER_ID' => $order_id];
-        }
+        // No existe, generar uno nuevo
+        $rand = random_int(100000, PHP_INT_MAX);
+        $hashes[] = ['HASH' => $rand, 'ORDER_ID' => $order_id];
 
         if (count($hashes) > 20) {
             $hashes = array_slice($hashes, -20);
         }
 
         self::saveStoredHashes($hashes);
-        self::$requestCache[$order_id] = $rand;
-
-        nacexutils::writeNacexLog('hash_form :: order_id: ' . $order_id . ' | hash: ' . $rand . ' | session_id: ' . session_id());
-
         return $rand;
     }
 
@@ -61,26 +48,20 @@ class hash
         $order_id = Tools::getValue('order_id');
         $hash = Tools::getValue('hash');
 
-        nacexutils::writeNacexLog('validate_hash :: order_id: ' . $order_id . ' | hash_recibido: ' . $hash . ' | session_id: ' . session_id());
-
         if (!$order_id || !$hash) {
-            nacexutils::writeNacexLog('validate_hash :: FAIL - order_id o hash vacíos');
             return false;
         }
 
         $hashes = self::getStoredHashes();
-        nacexutils::writeNacexLog('validate_hash :: hashes en sesión: ' . json_encode($hashes));
 
         foreach ($hashes as $key => $entry) {
             if ($entry['ORDER_ID'] == $order_id && $entry['HASH'] == $hash) {
                 unset($hashes[$key]);
                 self::saveStoredHashes(array_values($hashes));
-                nacexutils::writeNacexLog('validate_hash :: OK');
                 return true;
             }
         }
 
-        nacexutils::writeNacexLog('validate_hash :: FAIL - hash no encontrado');
         return false;
     }
 }
